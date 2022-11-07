e91c5c7fc2225bb8f957c2b2e5969d3059761ff4
Jeremy <jeremyvillalva66@gmail.com>
2022-11-07 18:01:28 -0500

    font increased, mobile

--- art-app/src/Components/Header.jsx
+++ art-app/src/Components/Header.jsx
@@ -4,7 +4,8 @@ export default function Header() {
     return (
         <div>
             <div className="logo-container">
-            <h1 className="logo"> Amiibo Encyclopedia </h1>
+            <h1 class="animate__animated animate__bounce"
+            id="logo"> Amiibo Encyclopedia </h1>
             </div>
             <div className="header-nav">
                 <Nav />
--- art-app/src/Components/Home.jsx
+++ art-app/src/Components/Home.jsx
@@ -2,10 +2,9 @@ export default function Home() {
     return (
         <div> 
             <h1> welcome to the unofficial amiibo encylcopedia </h1>
-            <a  href="https://www.amiiboapi.com/"> this site was built with the amiibo api </a>
       
-
             <footer>
+            <a  href="https://www.amiiboapi.com/"> this site was built with the amiibo api </a>
                 <p> this website was built by Jbillaba </p>
             </footer>
 

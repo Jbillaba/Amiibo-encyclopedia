1f829ddd49152400c9829f82dcee2e28c2fbf9dc
Jeremy <jeremyvillalva66@gmail.com>
2022-11-08 14:59:02 -0500

    run build

--- art-app/src/Components/Home.jsx
+++ art-app/src/Components/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <div> 
             <h1> welcome to the unofficial amiibo encylcopedia </h1>
-      
+            <img src='' alt='amiibo'/>
             <footer>
             <a  href="https://www.amiiboapi.com/"> this site was built with the amiibo api </a>
                 <p> this website was built by Jbillaba </p>
